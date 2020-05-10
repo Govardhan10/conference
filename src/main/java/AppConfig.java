9afce5b1a3363f730d39a762d2a4ab8f8c380643
Govardhan <govardhan.yannam@gmail.com>
2020-05-10 21:41:23 +0530

    Factory Bean demonstration

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,8 +1,25 @@
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import util.CalendarFactory;
 
+import java.util.Calendar;
+
+@Configuration
 @ComponentScan({"com.pluralsight"})
 public class AppConfig {
 
+    @Bean(name = "cal")
+    public CalendarFactory calendarFactory() {
+        CalendarFactory factory = new CalendarFactory();
+        factory.addDays(2);
+        return factory;
+    }
+
+    @Bean
+    public Calendar cal() throws Exception {
+        return calendarFactory().getObject();
+    }
     /*
     @Bean(name= "speakerRepository")
     public SpeakerRepository getSpeakerRepository() {
--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -3,7 +3,6 @@ package com.pluralsight.service;
 import com.pluralsight.model.Speaker;
 import com.pluralsight.repository.SpeakerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
