874bd43a16f1f6d7f3aa22842a558cc172888415
Govardhan <govardhan.yannam@gmail.com>
2020-05-11 00:27:32 +0530

    Spring bean profiles

--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -3,11 +3,17 @@ package com.pluralsight.service;
 import com.pluralsight.model.Speaker;
 import com.pluralsight.repository.SpeakerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service("speakerService")
+@Profile("dev")
+/*
+ To configure application to run for specific profile , following parameter need to be added to JVM options
+ -Dspring.profiles.active=dev
+ */
 public class SpeakerServiceImpl implements SpeakerService {
 
     private SpeakerRepository repository;
