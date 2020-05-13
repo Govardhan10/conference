81ef8b7940859de672202dbce9681a8471b46610
Govardhan <govardhan.yannam@gmail.com>
2020-05-13 08:37:50 +0530

    for packing application as war file

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/*
+In case of WAR deployments we have to extend our application to generate
+the context needed for a WAR deployment
+public class Application extends SpringBootServletInitializer {
+ */
 public class Application {
 
     public static void main(String[] args) {
